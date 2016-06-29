8d24603a9765f52f068c5c5d44db3e62a40c2aa4
AssassinFamilyValues <megamangafan1@gmail.com>
2016-06-29 10:16:21 +1000

    new structure

---
+++ script.js
@@ -0,0 +1,22 @@
+var divs = ["chapter1", "chapter2", "chapter3", "chapter4", "chapter5"];
+ var visibleDivId = null;
+ function toggleVisibility(divId) {
+ if(visibleDivId === divId) {
+ //visibleDivId = null;
+ } else {
+ visibleDivId = divId;
+ }
+ hideNonVisibleDivs();
+ }
+ function hideNonVisibleDivs() {
+ var i, divId, div;
+ for(i = 0; i < divs.length; i++) {
+ divId = divs[i];
+ div = document.getElementById(divId);
+ if(visibleDivId === divId) {
+ div.style.display = "block";
+ } else {
+ div.style.display = "none";
+ }
+ }
+ }
